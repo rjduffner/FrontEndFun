bdc47c3c83bdffde84703ac03235461332bdfd4c
rjduffner <rjduffner@gmail.com>
2014-03-08 20:42:23 -0800

    setup commit

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -2,22 +2,13 @@ from pyramid.config import Configurator
 from pyramid_jinja2 import renderer_factory
 from webapp.models import get_root
 
+
 def main(global_config, **settings):
-    """ This function returns a WSGI application.
-    
-    It is usually called by the PasteDeploy framework during 
-    ``paster serve``.
-    """
-    settings = dict(settings)
-    settings.setdefault('jinja2.i18n.domain', 'webapp')
-
-    config = Configurator(root_factory=get_root, settings=settings)
-    config.add_translation_dirs('locale/')
+    """ This function returns a Pyramid WSGI application.
+"""
+    config = Configurator(settings=settings)
+    config.add_static_view('static', 'static', cache_max_age=3600)
     config.include('pyramid_jinja2')
-
-    config.add_static_view('static', 'static')
-    config.add_view('webapp.views.my_view',
-                    context='webapp.models.MyModel', 
-                    renderer="mytemplate.jinja2")
-
+    config.add_route('home', '/')
+    config.scan()
     return config.make_wsgi_app()
--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,16 @@
-from pyramid.i18n import TranslationStringFactory
+"""
 
-_ = TranslationStringFactory('webapp')
+Author: Robert Duffner
+Date: March 8, 2014
+Email: rjduffner@gmail.com
 
-def my_view(request):
+views.py
+
+"""
+from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+
+
+@view_config(route_name='home', renderer='templates/home.jinja2')
+def home(request):
     return {'project':'webapp'}
